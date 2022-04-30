395180019efea2f138d4d5f1e1f8ae2ed50f58b5
Samuel <smltrs0@gmail.com>
2022-04-30 17:42:31 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 // Example config for adding a loader that depends on babel-loader
 // This source was taken from the @next/mdx plugin source:
 // https://github.com/vercel/next.js/tree/canary/packages/next-mdx
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/me/' : ''
+}
+
 module.exports = {
     webpack: (config, options) => {
         config.module.rules.push({
