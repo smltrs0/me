42853293e7cacf47b1095f4ce95c3df93c910226
Samuel <smltrs0@gmail.com>
2022-04-30 17:17:10 -0400

    first commit

---
+++ pages/HomeComponents/ButtonDarkMode.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+export default function ButtonDarkMode({styles, darkMode, setDarkMode}){
+    return <>
+        <div className={styles.buttonDarkMode} onClick={()=>{ setDarkMode(!darkMode) }}>
+            {darkMode ? 
+                <img src="https://img.icons8.com/color/48/000000/sun.png" alt="Sun"/>
+                :
+                <img src="https://img.icons8.com/color/48/000000/moon.png" alt="Moon"/>
+            }
+        </div>
+    </>
+}
+
