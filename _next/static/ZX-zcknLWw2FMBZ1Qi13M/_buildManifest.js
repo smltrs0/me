eed65b8025bc38e9472adda499f398c0c790ac69
Samuel <smltrs0@gmail.com>
2022-05-01 23:06:53 -0400

    fix

---
+++ _next/static/ZX-zcknLWw2FMBZ1Qi13M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/46-1a6ed02ca6ac891c.js","static/css/4a696bfda1b85501.css","static/chunks/pages/index-b0f85a161e7d3abf.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
