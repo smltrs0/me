3564d5bab010ea68f62074faca908d72496b31b7
Samuel <49120338+smltrs0@users.noreply.github.com>
2022-09-17 10:23:31 -0400

    Delete _next/static directory

--- _next/static/yywS1SxATgmVyOeMLyKlo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/46-1a6ed02ca6ac891c.js","static/css/acb618c44ca99102.css","static/chunks/pages/index-90f823eb116bbd47.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
